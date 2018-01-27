bc6d161e004bf17a5b06dca817d8166152e970f1
Darsh Naik <naik.darsh729@gmail.com>
2018-01-27 22:17:31 +0530

    Updated

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 source = json.load(open("data.json"))
 
@@ -6,6 +7,14 @@ def meaning(word):
     word = word.lower()
     if word in source:
         return source[word]
+    elif len(get_close_matches(word,source.keys(),cutoff=0.8))>0:
+        ans = input("Did you mean %s instead?(Y/n) : " % get_close_matches(word,source.keys())[0])
+        if ans.lower() == "y":
+            return source[get_close_matches(word,source.keys())[0]]
+        elif ans.lower() == 'n':
+            return "The Word is not defined; or perhaps you must be joking(Nice One!!^_^)"
+        else:
+            return "You are Stupid!!"
     else:
         return "The Word is not defined; or perhaps you must be joking(Nice One!!^_^)"
 
